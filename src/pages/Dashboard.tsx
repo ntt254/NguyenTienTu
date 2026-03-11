dbe2350a0bcb8756fd7db297ccf08a7d72a120ea
Tu <tienvinh1611@gmail.com>
2026-03-11 14:21:10 +0700

    1

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,50 +1,50 @@
 import { Toaster } from "react-hot-toast";
-import { Link, Route, Routes } from "react-router-dom";
-import Register from "./Register";
-
+import { Link, Route, Routes , Navigate } from "react-router-dom";
+import { Signin } from "./signin";
+import { Signup } from "./signup";
+import { Userlist } from "./userlist";
 const Dashboard = () => {
     return (
-    <>
-      <nav className="bg-blue-600 text-white shadow">
-        <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
-          <Link to="#" className="text-xl font-semibold">
-            <strong>WEB2091 App</strong>
-          </Link>
+        <>
+            <nav className="bg-blue-600 text-white shadow">
+                <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+                    <Link to="#" className="text-xl font-semibold">
+                        <strong>WEB2091 App</strong>
+                    </Link>
 
-          <div className="hidden md:flex items-center space-x-8">
-            <Link to="#" className="hover:text-gray-200">
-              Trang chủ
-            </Link>
-            <Link to="/list" className="hover:text-gray-200">
-              Danh sách
-            </Link>
-            <Link to="/add" className="hover:text-gray-200">
-              Thêm mới
-            </Link>
-          </div>
+                    <div className="hidden md:flex items-center space-x-8">
+                        <Link to="#" className="hover:text-gray-200">
+                            Trang chủ
+                        </Link>
+                        <Link to="/userlist" className="hover:text-gray-200">
+                            Danh sách
+                        </Link>
+                        <Link to="/add" className="hover:text-gray-200">
+                            Thêm mới
+                        </Link>
+                    </div>
 
-          <div className="hidden md:flex items-center space-x-6">
-            <Link to="#" className="hover:text-gray-200">
-              Đăng nhập
-            </Link>
-            <Link to="/register" className="hover:text-gray-200">
-              Đăng ký
-            </Link>
-          </div>
-        </div>
-      </nav>
+                    <div className="hidden md:flex items-center space-x-6">
+                        <Link to="/signin" className="hover:text-gray-200">
+                            Đăng nhập
+                        </Link>
+                        <Link to="/signup" className="hover:text-gray-200">
+                            Đăng ký
+                        </Link>
+                    </div>
+                </div>
+            </nav>
 
-      {/* MAIN CONTENT */}
-      <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
-        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
-      </div>
-      <Routes>
-
-        <Route path="/register" element={<Register/>} />
-      </Routes>
-      <Toaster />
-    </>
-    );
+            {/* MAIN CONTENT */}
+            
+            <Routes>
+                <Route path="/" element={<Navigate to="/userlist"/>}></Route>
+                <Route path="/signup" element={<Signup></Signup>}></Route>
+                <Route path="/signin" element={<Signin></Signin>}></Route>
+                <Route path="/userlist" element={<Userlist></Userlist>}></Route>
+            </Routes>
+            <Toaster />
+        </>
+    )
 }
-
-export default Dashboard;
+export default Dashboard
